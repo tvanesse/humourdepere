9c01ab2d0e13ae953776f52d642b453569c15705
Thomas Vanesse <vanessethomas@gmail.com>
2015-06-13 16:11:58 +0200

    Initial commit.

---
+++ www/js/app.js
@@ -0,0 +1,26 @@
+angular.module('hdp-mobile', [
+  'ionic',
+  'hdp.controllers'
+  ])
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise("/");
+  $stateProvider
+    .state('main', {
+      url: '/',
+      templateUrl: '../templates/main.html'
+    })
+})
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
---
+++ www/js/controllers.js
@@ -0,0 +1,1 @@
+angular.module('hdp.controllers', ['hdp.services'])
